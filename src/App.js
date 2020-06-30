2b5fc2adaf9c9ad49721121b9af42132ad071a28
Rosaria DeBiasio <rosaria.debiasio@sourcesense.com>
2020-06-30 18:31:15 +0200

    add keys

--- src/App.js
+++ src/App.js
@@ -19,15 +19,15 @@ function App() {
       <p className="text-center m-16">Cart Project</p>
       <div className="grid grid-cols-4 gap-8 m-20">
         {products.map((product, index) => (
-          <div className="grid grid-rows-3 grid-flow-col gap-4">
+          <div key={index} className="grid grid-rows-3 grid-flow-col gap-4">
           <img className="row-span-3 col-span-1"
-            key={index}
+            key={product.name}
             src="https://via.placeholder.com/250"
             alt={product.name}
           />
-          <span className="row-span-1 col-span-2">{product.prezzo}</span>
-          <button className="row-span-1 col-span-2 text-left">Apri dettaglio</button>
-          <button className="row-span-1 col-span-2">Aggiungi al carrello</button>
+          <span key={product.prezzo} className="row-span-1 col-span-2">{product.prezzo}</span>
+          <button key={product.id} className="row-span-1 col-span-2 text-left">Apri dettaglio</button>
+          <button key={`btn ${product.id}`} className="row-span-1 col-span-2">Aggiungi al carrello</button>
           </div>
         ))}
       </div>
