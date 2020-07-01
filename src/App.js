77c681ce2968e87159a5a5144012a163ba5ea0a8
Rosaria DeBiasio <rosaria.debiasio@sourcesense.com>
2020-07-01 14:38:44 +0200

    structuring our products

--- src/App.js
+++ src/App.js
@@ -15,23 +15,36 @@ function App() {
   }, [setProducts]);
 
   return products.length > 0 ? (
-    <div className="container mx-auto">
-      <p className="text-center m-16">Cart Project</p>
-      <div className="grid grid-cols-4 gap-8 m-20">
-        {products.map((product, index) => (
-          <div key={index} className="grid grid-rows-3 grid-flow-col gap-4">
-          <img className="row-span-3 col-span-1"
-            key={product.name}
-            src="https://via.placeholder.com/250"
-            alt={product.name}
-          />
-          <span key={product.prezzo} className="row-span-1 col-span-2">{product.prezzo}</span>
-          <button key={product.id} className="row-span-1 col-span-2 text-left">Apri dettaglio</button>
-          <button key={`btn ${product.id}`} className="row-span-1 col-span-2">Aggiungi al carrello</button>
-          </div>
-        ))}
+    <>
+      <header className="bg-gray-800 text-white flex justify-between">
+        <span className="ml-20">Logo</span>
+        <span className="mr-20">Cart</span>
+      </header>
+      <h1 className="text-center text-6xl bg-gray-800 text-white mb-12">
+        Our Products
+      </h1>
+      <div className="flex justify-between">
+      <div className="bg-gray-200 ml-20 w-1/5">Search Filters</div>
+      <div className="container mr-20">
+        <div className="grid grid-cols-4 gap-5">
+          {products.map((product, index) => (
+            <div key={index} className="flex flex-col">
+              <img
+                className=""
+                src="https://via.placeholder.com/300"
+                alt={product.name}
+              />
+              <span className="">{`$ ${product.price}`}</span>
+              <div className="flex justify-between">
+                <button className="">Open details</button>
+                <button className="">Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+      </div>
+    </>
   ) : (
     <p className="text-center m-16">Loading...</p>
   );
--- src/server.js
+++ src/server.js
@@ -7,44 +7,44 @@ export default function () {
       this.get("/api/products", () => ({
         products: [{
 					id: 1,
-					name: faker.commerce.productName(),
-					prezzo: faker.commerce.price(),
-					immagine: faker.image.image()
+					name: 'Product Name',
+					price: faker.commerce.price(),
+					image: faker.image.image()
 				}, {
 					id: 2,
-					name: faker.commerce.productName(),
-					prezzo: faker.commerce.price(),
-					immagine: faker.image.image()
+					name: 'Product Name',
+					price: faker.commerce.price(),
+					image: faker.image.image()
 				}, {
 					id: 3,
-					name: faker.commerce.productName(),
-					prezzo: faker.commerce.price(),
-					immagine: faker.image.image()
+					name: 'Product Name',
+					price: faker.commerce.price(),
+					image: faker.image.image()
 				}, {
 					id: 4,
-					name: faker.commerce.productName(),
-					prezzo: faker.commerce.price(),
-					immagine: faker.image.image()
+					name: 'Product Name',
+					price: faker.commerce.price(),
+					image: faker.image.image()
 				}, {
 					id: 5,
-					name: faker.commerce.productName(),
-					prezzo: faker.commerce.price(),
-					immagine: faker.image.image()
+					name: 'Product Name',
+					price: faker.commerce.price(),
+					image: faker.image.image()
 				}, {
 					id: 6,
-					name: faker.commerce.productName(),
-					prezzo: faker.commerce.price(),
-					immagine: faker.image.image()
+					name: 'Product Name',
+					price: faker.commerce.price(),
+					image: faker.image.image()
 				}, {
 					id: 7,
-					name: faker.commerce.productName(),
-					prezzo: faker.commerce.price(),
-					immagine: faker.image.image()
+					name: 'Product Name',
+					price: faker.commerce.price(),
+					image: faker.image.image()
 				}, {
 					id: 8,
-					name: faker.commerce.productName(),
-					prezzo: faker.commerce.price(),
-					immagine: faker.image.image()
+					name: 'Product Name',
+					price: faker.commerce.price(),
+					image: faker.image.image()
 				}],
       }))
     },
