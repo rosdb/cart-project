2fa7db0233fb899eb617c4a1dcfc32513ce55f94
Rosaria DeBiasio <rosaria.debiasio@sourcesense.com>
2020-07-01 19:31:37 +0200

    add observable

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
+import { Observable } from "rxjs";
+
+const useObservable = (observable, setter) => {
+  useEffect(() => {
+    let subscription = observable.subscribe((value) => setter(value));
+    return () => subscription.unsubscribe();
+  }, [observable, setter]);
+};
+
+let products$ = Observable.create((obs) => {
+  fetch("/api/products")
+    .then((res) => res.json())
+    .then((data) => data.products)
+    .then((p) => {
+      obs.next(p);
+      obs.complete();
+    })
+    .catch((err) => obs.error(err));
+});
 
 function App() {
   let [products, setProducts] = useState([]);
 
-  const fetchProd = async () => {
-    const response = await fetch("/api/products");
-    const data = await response.json();
-    return data.products;
-  };
-
-  useEffect(() => {
-    fetchProd().then((data) => setProducts(data));
-  }, [setProducts]);
+  useObservable(products$, setProducts);
 
   return products.length > 0 ? (
     <>
@@ -24,25 +34,25 @@ function App() {
         Our Products
       </h1>
       <div className="flex justify-between">
-      <div className="bg-gray-200 ml-20 w-1/5">Search Filters</div>
-      <div className="container mr-20">
-        <div className="grid grid-cols-4 gap-5">
-          {products.map((product, index) => (
-            <div key={index} className="flex flex-col">
-              <img
-                className=""
-                src="https://via.placeholder.com/300"
-                alt={product.name}
-              />
-              <span className="">{`$ ${product.price}`}</span>
-              <div className="flex justify-between">
-                <button className="">Open details</button>
-                <button className="">Add to cart</button>
+        <div className="bg-gray-200 ml-20 w-1/5">Search Filters</div>
+        <div className="container mr-20">
+          <div className="grid grid-cols-4 gap-5">
+            {products.map((product, index) => (
+              <div key={index} className="flex flex-col">
+                <img
+                  className=""
+                  src="https://via.placeholder.com/300"
+                  alt={product.name}
+                />
+                <span className="">{`$ ${product.price}`}</span>
+                <div className="flex justify-between">
+                  <button className="">Open details</button>
+                  <button className="">Add to cart</button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
       </div>
     </>
   ) : (
