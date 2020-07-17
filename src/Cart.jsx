240a60c9d4f8ede475f40e76ed30f00403a2b29e
Rosaria DeBiasio <rosaria.debiasio@sourcesense.com>
2020-07-17 16:04:07 +0200

    add icons

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Cart(props) {
   const {cart, removeToCart, totalPrice, cartView} = props;
@@ -12,8 +13,6 @@ function Cart(props) {
           : 'bg-gray-200 m-4 flex flex-col cart p-2 hidden'
       }
     >
-      <h3 className="text-center mb-4">Cart</h3>
-
       {cart.map(item => (
         <div className="flex flex-col" key={item.id}>
           <div className="flex justify-between items-center mb-2">
@@ -39,14 +38,36 @@ function Cart(props) {
                 ></path>
               </g>
             </svg>
-            <span>{item.name}</span>
+            <Link to={`/product/${item.id}`}>
+              <span>{item.name}</span>
+            </Link>
             <span>{`Qty ${item.qty}`}</span>
           </div>
-          <span className="self-end mb-2">{`$${item.price}`}</span>
+          <span className="self-end mb-3">{`$${item.price}`}</span>
         </div>
       ))}
-      <span className="self-end mb-4">Totale: {`$${totalPrice}`}</span>
-      <button className="self-end mb-6">Checkout</button>
+      <div className="text-center mb-6">
+        Total cart:
+        <span className="text-xl font-semibold">{` $${totalPrice}`}</span>
+      </div>
+      <div className="cursor-pointer self-center flex">
+        <span className="self-end p-1">Checkout</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="32px"
+          height="32px"
+          viewBox="0 0 32 32"
+        >
+          <g transform="translate(0, 0)">
+            <path
+              fill="#444444"
+              d="M30.707,15.293L21,5.586L19.586,7l8,8H1v2h26.586l-8,8L21,26.414l9.707-9.707 C31.098,16.316,31.098,15.684,30.707,15.293z"
+            ></path>
+          </g>
+        </svg>
+      </div>
     </div>
   );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -15,11 +15,44 @@ function List({products, addToCart}) {
             <span className="">{`$ ${product.price}`}</span>
             <div className="flex justify-between">
               <Link to={`/product/${product.id}`}>
-                <button className="">Open details</button>
+                <button className="">{product.name}</button>
               </Link>
-              <button className="" onClick={() => addToCart(product)}>
-                Add to cart
-              </button>
+              <svg
+                className="cursor-pointer"
+                onClick={() => addToCart(product)}
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="16px"
+                height="16px"
+                viewBox="0 0 16 16"
+              >
+                <g transform="translate(0, 0)">
+                  <path
+                    fill="#444444"
+                    d="M13,11H3c-0.553,0-1-0.448-1-1V2H0V0h3c0.553,0,1,0.448,1,1v8h8.246l1.428-5H12V2h3 c0.917,0,1.048,0.973,0.962,1.275l-2,7C13.839,10.704,13.446,11,13,11z"
+                  ></path>{' '}
+                  <circle
+                    data-color="color-2"
+                    fill="#444444"
+                    cx="3"
+                    cy="14"
+                    r="2"
+                  ></circle>{' '}
+                  <circle
+                    data-color="color-2"
+                    fill="#444444"
+                    cx="13"
+                    cy="14"
+                    r="2"
+                  ></circle>{' '}
+                  <polygon
+                    data-color="color-2"
+                    fill="#444444"
+                    points="9,5 9,1 7,1 7,5 5,5 8,8 11,5 "
+                  ></polygon>
+                </g>
+              </svg>
             </div>
           </div>
         ))}
